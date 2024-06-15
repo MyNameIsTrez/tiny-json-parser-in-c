aae4caebcb79a7266f5dad6a66b9860b140935fe
MyNameIsTrez <welfje@gmail.com>
2024-06-15 16:47:34 +0200

    Add missing void arguments

--- tests.c
+++ tests.c
@@ -3,47 +3,47 @@
 #include <assert.h>
 #include <string.h>
 
-static void ok_string() {
+static void ok_string(void) {
 	struct json_node node;
 	assert(!json_parse("./tests_ok/string.json", &node));
 	assert(node.type == JSON_NODE_STRING);
 	assert(strcmp(node.data.string.str, "\"\"") == 0);
 }
 
-static void ok_array() {
+static void ok_array(void) {
 	struct json_node node;
 	assert(!json_parse("./tests_ok/array.json", &node));
 	assert(node.type == JSON_NODE_ARRAY);
 	assert(node.data.array.node_count == 0);
 }
 
-static void ok_object() {
+static void ok_object(void) {
 	struct json_node node;
 	assert(!json_parse("./tests_ok/object.json", &node));
 	assert(node.type == JSON_NODE_OBJECT);
 	assert(node.data.object.field_count == 0);
 }
 
-static void error_json_file_is_empty() {
+static void error_json_file_is_empty(void) {
 	assert(json_parse("./tests_err/empty.json", NULL) && json_error == JSON_ERROR_JSON_FILE_IS_EMPTY);
 }
 
-static void error_failed_to_open_json_file() {
+static void error_failed_to_open_json_file(void) {
 	assert(json_parse("", NULL) && json_error == JSON_ERROR_FAILED_TO_OPEN_JSON_FILE);
 }
 
-static void ok_tests() {
+static void ok_tests(void) {
 	ok_string();
 	ok_array();
 	ok_object();
 }
 
-static void error_tests() {
+static void error_tests(void) {
 	error_failed_to_open_json_file();
 	error_json_file_is_empty();
 }
 
-int main() {
+int main(void) {
 	error_tests();
 	ok_tests();
 }
